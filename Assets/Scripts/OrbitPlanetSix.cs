5bd3442c05c75d7493a4da421c38d28c314fe55a
FirestarterMKD <33908546+FirestarterMKD@users.noreply.github.com>
2019-09-16 11:32:25 +0200

    Planets Orbit Script

---
+++ Assets/Scripts/OrbitPlanetSix.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitPlanetSix : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    float rotationSpeed = 0.9f;
+    void Update()
+    {
+         transform.Rotate(0,rotationSpeed*Time.deltaTime,0);
+    }
+}
