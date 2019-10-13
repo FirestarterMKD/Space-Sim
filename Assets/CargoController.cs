3e34c640ac890b9c08139c7be0e33e5e4f31d668
noahbearx3 <noah.b3rkhout@gmail.com>
2019-10-13 10:52:12 +0200

    New Cargoship movement Script

---
+++ Assets/CargoController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoController : MonoBehaviour
+{
+    public float speed = 15f;
+    GameObject CargoShip;
+    GameObject Planet;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CargoShip = GameObject.FindWithTag("Cargo");
+        Planet = GameObject.FindWithTag("PlanetOne");
+
+        target = Planet.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step =  speed * Time.deltaTime; 
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+    }
+}
