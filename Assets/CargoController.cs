1894016533aa842cd467d53656419337c66f1a2e
noahbearx3 <noah.b3rkhout@gmail.com>
2019-10-18 12:15:19 +0200

    Cargo Controller Update
    Cargo now needs fuel

--- Assets/CargoController.cs
+++ Assets/CargoController.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class CargoController : MonoBehaviour
 {
     public float speed = 15f;
+    public float maxIron = 5000;
+    public float maxUranium = 1000;
+    public float cargoIron = 0;
+    public float cargoUranium = 0;
+    public float cargoFuel = 50;
+    public float minFuel = 1;
+    public float maxFuel = 1000;
+
     GameObject CargoShip;
     GameObject Planet;
 
@@ -17,15 +25,25 @@ public class CargoController : MonoBehaviour
         Planet = GameObject.FindWithTag("PlanetOne");
 
         target = Planet.transform;
+        cargoFuel = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float step =  speed * Time.deltaTime; 
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        
+            sendCargo();
+    }
+       
+    void sendCargo()
+    {
 
+        if (cargoFuel > minFuel)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
     }
 
-    
+
 }
