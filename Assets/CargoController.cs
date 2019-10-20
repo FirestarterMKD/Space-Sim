d01468898c51cab3803fec0b0d776820017d3d18
noahbearx3 <noah.b3rkhout@gmail.com>
2019-10-20 02:31:31 +0200

    CargoController
    CargoController Update Includes:
    Moving to the each planet numbered from [0-7](Not being able to pass the sun must be fixed)
    Players Iron And Uranium gets returned if too much is added to the cargo along with an Error Message

--- Assets/CargoController.cs
+++ Assets/CargoController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CargoController : MonoBehaviour
 {
+
+    public float playerIron = 0;
+    public float playerUranium = 0;
     public float speed = 15f;
     public float maxIron = 5000;
     public float maxUranium = 1000;
@@ -12,6 +15,7 @@ public class CargoController : MonoBehaviour
     public float cargoFuel = 50;
     public float minFuel = 1;
     public float maxFuel = 1000;
+
 
     GameObject CargoShip;
     GameObject Planet;
@@ -48,14 +52,18 @@ public class CargoController : MonoBehaviour
         checkCargo();
         sendCargo();
         sendCargo2();
-
+        sendCargo3();
+        sendCargo4();
+        sendCargo5();
+        sendCargo6();
+        sendCargo7();
 
     }
 
     void sendCargo()
     {
         target = Planet.transform;
-        if (Input.GetKey("space"))
+        if (Input.GetKey("1"))
         {
             if (cargoFuel > minFuel)
             {
@@ -65,12 +73,12 @@ public class CargoController : MonoBehaviour
         }
     }
 
-    
+
 
     void sendCargo2()
     {
         target = PlanetTwo.transform;
-        if (Input.GetKey("backspace "))
+        if (Input.GetKey("2"))
         {
             if (cargoFuel > minFuel)
             {
@@ -79,6 +87,71 @@ public class CargoController : MonoBehaviour
             }
         }
     }
+
+    void sendCargo3()
+    {
+        target = PlanetThree.transform;
+        if (Input.GetKey("3"))
+        {
+            if (cargoFuel > minFuel)
+            {
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            }
+        }
+    }
+
+    void sendCargo4()
+    {
+        target = PlanetFour.transform;
+        if (Input.GetKey("4"))
+        {
+            if (cargoFuel > minFuel)
+            {
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            }
+        }
+    }
+
+    void sendCargo5()
+    {
+        target = PlanetFive.transform;
+        if (Input.GetKey("5"))
+        {
+            if (cargoFuel > minFuel)
+            {
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            }
+        }
+    }
+
+    void sendCargo6()
+    {
+        target = PlanetSix.transform;
+        if (Input.GetKey("6"))
+        {
+            if (cargoFuel > minFuel)
+            {
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            }
+        }
+    }
+    void sendCargo7()
+    {
+        target = PlanetSeven.transform;
+        if (Input.GetKey("7"))
+        {
+            if (cargoFuel > minFuel)
+            {
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            }
+        }
+    }
+
 
 
 
@@ -86,23 +159,25 @@ public class CargoController : MonoBehaviour
 
 
     void checkCargo()
+    {
+
+        if (cargoIron > maxIron)
         {
-
-            if (cargoIron > maxIron)
-            {
-                cargoIron = maxIron;
-                print("That is too much Iron!!");
-            }
-
-            if (cargoUranium > maxUranium)
-            {
-                cargoUranium = maxUranium;
-                print("That is too much Uranium!!");
-
-
-            }
+            playerIron = cargoIron - maxIron;
+            cargoIron = maxIron;
+            print("That is too much Iron!!");
         }
 
-    
+        if (cargoUranium > maxUranium)
+        {
+            playerUranium = cargoUranium - maxUranium;
+            cargoUranium = maxUranium;
+            print("That is too much Uranium!!");
+
+
+        }
+    }
 
 }
+
+
