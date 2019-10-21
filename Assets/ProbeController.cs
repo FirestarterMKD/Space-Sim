ea33e2e6a0f4f8c1339d7db59e86a499712ba492
noahbearx3 <noah.b3rkhout@gmail.com>
2019-10-21 20:56:30 +0200

    Probe will show resources when reached

--- Assets/ProbeController.cs
+++ Assets/ProbeController.cs
@@ -5,27 +5,149 @@ using UnityEngine;
 public class ProbeController : MonoBehaviour
 
 {
+    public GameObject canvasObject1;
+    public GameObject canvasObject2;
+    public GameObject canvasObject3;
+    public GameObject canvasObject4;
+    public GameObject canvasObject5;
+    public GameObject canvasObject6;
+    public GameObject canvasObject7;
+
     public float speed = 15f;
-    GameObject CargoShip;
-    GameObject ColliderPlanet7;
+    GameObject Planet;
+    GameObject PlanetTwo;
+    GameObject PlanetThree;
+    GameObject PlanetFour;
+    GameObject PlanetFive;
+    GameObject PlanetSix;
+    GameObject PlanetSeven;
 
     private Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
-        CargoShip = GameObject.FindWithTag("Cargo");
-        ColliderPlanet7 = GameObject.FindWithTag("PlanetSeven");
 
-        target = ColliderPlanet7.transform;
+        Planet = GameObject.FindWithTag("PlanetOne");
+        PlanetTwo = GameObject.FindWithTag("PlanetTwo");
+        PlanetThree = GameObject.FindWithTag("PlanetThree");
+        PlanetFour = GameObject.FindWithTag("PlanetFour");
+        PlanetFive = GameObject.FindWithTag("PlanetFive");
+        PlanetSix = GameObject.FindWithTag("PlanetSix");
+        PlanetSeven = GameObject.FindWithTag("PlanetSeven");
+
+        canvasObject1.SetActive(false);
+        canvasObject2.SetActive(false);
+        canvasObject3.SetActive(false);
+        canvasObject4.SetActive(false);
+        canvasObject5.SetActive(false);
+        canvasObject6.SetActive(false);
+        canvasObject7.SetActive(false);
+
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        checkPlanet1();
+        checkPlanet2();
+        checkPlanet3();
+        checkPlanet4();
+        checkPlanet5();
+        checkPlanet6();
+        checkPlanet7();
 
+    }
+
+    void checkPlanet1()
+    {
+        target = Planet.transform;
+        if (Input.GetKey("1"))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+        if (Vector3.Distance(transform.position, target.position) < 0.001f)
+        {
+            canvasObject1.SetActive(true);
+            canvasObject2.SetActive(false);
+            canvasObject3.SetActive(false);
+            canvasObject4.SetActive(false);
+            canvasObject5.SetActive(false);
+            canvasObject6.SetActive(false);
+            canvasObject7.SetActive(false);
+        }
+    }
+
+    void checkPlanet2()
+    {
+        target = PlanetTwo.transform;
+        if (Input.GetKey("2"))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+    }
+
+    void checkPlanet3()
+    {
+        target = PlanetThree.transform;
+        if (Input.GetKey("3"))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+    }
+
+    void checkPlanet4()
+    {
+        target = PlanetFour.transform;
+        if (Input.GetKey("4"))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+    }
+
+    void checkPlanet5()
+    {
+        target = PlanetFive.transform;
+        if (Input.GetKey("5"))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+    }
+
+    void checkPlanet6()
+    {
+        target = PlanetSix.transform;
+        if (Input.GetKey("6"))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+    }
+
+    void checkPlanet7()
+    {
+        target = PlanetSeven.transform;
+        if (Input.GetKey("7"))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
     }
 
 
