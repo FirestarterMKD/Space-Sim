720780d5ff08e06ed07094d5e70034bd7597e895
lilmully <gokhanmullaoglu@gmail.com>
2019-10-22 14:58:55 +0200

    MoreUI

--- Assets/Scripts/menuu/PlayerPlanetScript.cs
+++ Assets/Scripts/menuu/PlayerPlanetScript.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerPlanetScript : MonoBehaviour
 {
+
+    public Text playerIronText;
+    public Text playerFuelText;
+    public Text playerUraniumText;
+    public Text playerCurrencyText;
+
     public static int iron = 500;
     public static int currency = 0;
     public static int fuel = 0;
@@ -17,6 +24,9 @@ public class PlayerPlanetScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerIronText.text = "IRON : " + iron;
+        playerFuelText.text = "FUEL : " + fuel;
+        playerUraniumText.text = "URANIUM : " + uranium;
+        playerCurrencyText.text = "CURRENCY : " + currency;
     }
 }
