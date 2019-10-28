5fd551348082676a53c2e82c7d970ddd59262c98
FirestarterMKD <33908546+FirestarterMKD@users.noreply.github.com>
2019-10-28 14:39:04 +0100

    Material swap script

---
+++ Assets/Scripts/MaterialSwap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwap : MonoBehaviour
+{
+    private Camera myCamera;
+    public float maxAltitude;
+    public GameObject planet;
+    public Material Outline;
+    public Material GasGiant1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float camAltitude = transform.position.y;
+
+        if (camAltitude > maxAltitude)
+        {
+            planet.GetComponent<MeshRenderer>().material = Outline;
+        } else if (camAltitude < maxAltitude) {
+            planet.GetComponent<MeshRenderer>().material = GasGiant1;
+        }
+    }
+}
